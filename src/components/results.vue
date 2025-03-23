f26324474eea58c2ddde7b0d5fd42c96f0c96ff7
High-Petrushka <daniilprokopovic12@gmail.com>
2025-03-23 18:36:24 +0300

    Adding a filtering to the result section

--- src/components/results.vue
+++ src/components/results.vue
@@ -1,6 +1,14 @@
 <script setup>
+import { reactive } from 'vue';
+
 const props = defineProps({
     resultList: Array,
+});
+
+const local = reactive({
+    amountCondition: 'none',
+    speedCondition: 'none',
+    sizeCondition: 'none',
 });
 </script>
 
@@ -25,25 +33,140 @@ const props = defineProps({
                                 <p>Total result</p>
                             </div>
                         </div>
-                        <div 
-                        v-for="(item) in props.resultList"
-                        class="table__row">
-                            <div class="table__tile">
-                                <p>{{ item.options.amount }}</p>
+                        <template v-for="(item) in props.resultList">
+                            <div
+                            v-if="(item.options.amount == local.amountCondition ||
+                                item.options.speed == local.speedCondition ||
+                                item.options.size == local.sizeCondition
+                            ) || (
+                                local.amountCondition === 'none' &&
+                                local.speedCondition === 'none' &&
+                                local.sizeCondition === 'none'
+                            )"
+                            class="table__row">
+                                <div class="table__tile">
+                                    <p>{{ item.options.amount }}</p>
+                                </div>
+                                <div class="table__tile">
+                                    <p>{{ item.options.speed }}</p>
+                                </div>
+                                <div class="table__tile">
+                                    <p>{{ item.options.size }}</p>
+                                </div>
+                                <div class="table__tile">
+                                    <p>{{ item.res }}</p>
+                                </div>
                             </div>
-                            <div class="table__tile">
-                                <p>{{ item.options.speed }}</p>
+                        </template>
+                    </div>
+                </div>
+                <div class="filter__box">
+                    <div class="sort__cont">
+                        <div class="sort__title">
+                            <h3>Sort by amount</h3>
+                        </div>
+                        <div class="sort__params">
+                            <div class="param__box">
+                                <label for="amount">
+                                    <input class="sort__radio" type="radio" name="amount" 
+                                    value="5" v-model="local.amountCondition">
+                                    5
+                                </label>
                             </div>
-                            <div class="table__tile">
-                                <p>{{ item.options.size }}</p>
+                            <div class="param__box">
+                                <label for="amount">
+                                    <input class="sort__radio" type="radio" name="amount" 
+                                    value="10" v-model="local.amountCondition">
+                                    10
+                                </label>
                             </div>
-                            <div class="table__tile">
-                                <p>{{ item.res }}</p>
+                            <div class="param__box">
+                                <label for="amount">
+                                    <input class="sort__radio" type="radio" name="amount" 
+                                    value="15" v-model="local.amountCondition">
+                                    15
+                                </label>
+                            </div>
+                            <div class="param__box">
+                                <label for="amount">
+                                    <input class="sort__radio" type="radio" name="amount" 
+                                    value="none" v-model="local.amountCondition">
+                                    No filtering
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sort__cont">
+                        <div class="sort__title">
+                            <h3>Sort by speed</h3>
+                        </div>
+                        <div class="sort__params">
+                            <div class="param__box">
+                                <label for="speed">
+                                    <input class="sort__radio" type="radio" name="speed" 
+                                    value="Low" v-model="local.speedCondition">
+                                    Low
+                                </label>
+                            </div>
+                            <div class="param__box">
+                                <label for="speed">
+                                    <input class="sort__radio" type="radio" name="speed" 
+                                    value="Medium" v-model="local.speedCondition">
+                                    Medium
+                                </label>
+                            </div>
+                            <div class="param__box">
+                                <label for="speed">
+                                    <input class="sort__radio" type="radio" name="speed" 
+                                    value="High" v-model="local.speedCondition">
+                                    High
+                                </label>
+                            </div>
+                            <div class="param__box">
+                                <label for="speed">
+                                    <input class="sort__radio" type="radio" name="speed" 
+                                    value="none" v-model="local.speedCondition">
+                                    No filtering
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sort__cont">
+                        <div class="sort__title">
+                            <h3>Sort by size</h3>
+                        </div>
+                        <div class="sort__params">
+                            <div class="param__box">
+                                <label for="size">
+                                    <input class="sort__radio" type="radio" name="size" 
+                                    value="Little" v-model="local.sizeCondition">
+                                    Little
+                                </label>
+                            </div>
+                            <div class="param__box">
+                                <label for="size">
+                                    <input class="sort__radio" type="radio" name="size" 
+                                    value="Medium" v-model="local.sizeCondition">
+                                    Medium
+                                </label>
+                            </div>
+                            <div class="param__box">
+                                <label for="size">
+                                    <input class="sort__radio" type="radio" name="size" 
+                                    value="Large" v-model="local.sizeCondition">
+                                    Large
+                                </label>
+                            </div>
+                            <div class="param__box">
+                                <label for="size">
+                                    <input class="sort__radio" type="radio" name="size" 
+                                    value="none" v-model="local.sizeCondition">
+                                    No filtering
+                                </label>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="filter__box"></div>
             </div>
         </div>
     </div>
@@ -55,6 +178,10 @@ const props = defineProps({
   grid-template-columns: 1fr;
   grid-template-rows: auto 1fr;
   row-gap: 4rem;
+}
+
+h3 {
+    font-size: 2rem;
 }
 
 .stat__cont {
@@ -94,7 +221,7 @@ const props = defineProps({
 
 .table__row:first-child {
     border-top: 2px solid var(--font-color);
-    border-bottom: 2px solid var(--font-color);
+    /* border-bottom: 2px solid var(--font-color); */
 }
 
 .table__tile {
@@ -109,7 +236,7 @@ const props = defineProps({
 }
 
 .table__title {
-    border-bottom: none;
+    /* border-bottom: none; */
     border-top-left-radius: 1rem;
     border-top-right-radius: 1rem;
 }
@@ -117,5 +244,33 @@ const props = defineProps({
 .table__row:last-child {
     border-bottom-left-radius: 1rem;
     border-bottom-right-radius: 1rem;
+}
+
+.filter__box {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, auto);
+    row-gap: 4rem;
+}
+
+.sort__cont {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto auto;
+    row-gap: 2rem;
+}
+
+.sort__title {
+    display: flex;
+    justify-content: center;
+}
+
+.param__box {
+    padding-left: 25%;
+}
+
+label {
+    width: fit-content;
+    margin-right: 0 auto;
 }
 </style>
